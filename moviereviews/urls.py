f6d81998e54c3d995a99c693f989b9ff9fab368a
paolavallejo <pvallej3@eafit.edu.co>
2024-02-26 15:49:11 -0500

    grafica de peliculas por año

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
-    path('signup/', movieViews.signup, name='signup'),
     path('news/', include('news.urls')),
+    path('statistics/', movieViews.statistics_view, name='statistics'),
+    path('signup/', movieViews.signup, name='signup'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
